32669351955a55bb49e186bcb6be187fb861faf7
Cristian Díaz <danubiodiaz2003edf@gmail.com>
2023-07-02 16:37:32 -0300

    Proyecto Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "bootstrap";
+// import "./style.css";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
+/*	
+	COMENTE ESTAS LINEAS PORQUE ME DEVOLVIA ESTE ERROR.
+	Uncaught SyntaxError: Cannot use import statement outside a module
+*/
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  	//write your code here
+
+  	let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+	let action = ['ate', 'peed', 'crushed', 'broke'];
+	let what = ['my homework', 'the keys', 'the car'];
+	let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+	const randomNumber = (min, max) => Math.floor((Math.random() * max) + min);
+
+	const getExcuse = () => {
+		const excuseHTML = document.getElementById("excuse");
+
+		let excuse = "";
+
+		excuse += who[randomNumber(0, 4)] + " ";
+		excuse += action[randomNumber(0, 4)] + " ";
+		excuse += what[randomNumber(0, 3)] + " ";
+		excuse += when[randomNumber(0, 5)];
+
+		excuseHTML.innerHTML = excuse;
+	}
+
+	getExcuse();
 };
